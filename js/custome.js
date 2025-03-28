f4d1754d86edd925da3f00607e65ba5518cd53cd
ravi0068 <ravisain2201@gmail.com>
2025-03-28 03:55:28 -0800

    Add files via upload

---
+++ js/custome.js
@@ -0,0 +1,41 @@
+$( document ).ready(function() {
+    $('.serviceslider').slick({
+        dots: false,
+        arrows:true,
+        infinite: true,
+        autoplay:true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              arrows:false
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+});
+
+
